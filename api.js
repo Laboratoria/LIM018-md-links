2da1726ce75ce25746f1e97bd8c738d8495dfb78
Deleina <marta.deleina@gmail.com>
2022-08-25 20:14:58 -0500

    Agregando funciones get link mdConvert

--- api.js
+++ api.js
@@ -1,60 +1,134 @@
 let fs = require('fs');
-const fsPromises = require('fs').promises;
-const readdir = require('node:fs/promises')
-const pathUtil = require('node:path');
+const path = require('path');
+const marked = require('marked');
+const cheerio = require('cheerio');
+//console.log(cheerio);
 
-
+//existsSync() => comprueba si un archivo existe, devuelve un valo booleano
 //validar si exciste la ruta 
-const existPath = (path) => fs.existsSync(path);
+const existPath = (road) => fs.existsSync(road);
 
+
+//path.resolve => resuelve una ruta y la convierte en una ruta absoluta
 //convertir ruta a absoluta
-const absolutePath = (path) => pathUtil.resolve(path);
+const absolutePath = (road) => path.resolve(road);
 
+
+//fs.statSync.isDirectory => verifica si el objeto fs describe un directorio retorna un valor booleano
 //verificar si es un directorio
-const pahtIsDirectory = (absolutePath) => fs.lstatSync(absolutePath).isDirectory();
+const pahtIsDirectory = (road) => fs.statSync(road).isDirectory();
 
+
+//fs.statSync.isFile => retorna imformacion sobre la ruta del archiv, retona un valor boolena 
 //vereificar  tipo de archivo
 const verifyFileType = (absolutePath) => fs.statSync(absolutePath).isFile();
 
-//verifica que archivos so tipo  .md
-const pathIsMd = (absolutePath) => pathUtil.extname(absolutePath);
+// path.extname => obtiene la porcin de extension de una ruta de archivo
+//verifica que archivos son tipo  .md
+const pathIsMd = (road) => path.extname(road);
 
+//fs.readdirSyn => lee el contenido de un directorio retorna un array de archivos u objetos
 //leer directorio
-const targetDir = (path) => {
-  fs.promises.readdir(path, 'utf8')
-    .then(filenames => {
-      for (let filename of filenames) {
-        console.log(filename)
-      }
-    })
-
-    // If promise is rejected
-    .catch(err => {
-      console.log(err)
-    })
-}
+const readDir = (road) => fs.readdirSync(road);
 
 
+//fs.readFileSync => lee archivos y devuelve su contenido
 //leer archivo
-const readFile = (absolutePath) => fs.promises.readFile(absolutePath)
-  .then(function (result) {
-    console.log('' + result);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
+const readFile = (absolutePath) => fs.readFileSync(absolutePath, 'utf-8');
 
 
-//obtener files.md
-const getFile = (path) => {
-  let arrayFile = [];
-  const pathAbsolute = absolutePath(path);
-  console.log(pathAbsolute);
-  if (verifyFileType(pathAbsolute) && pathIsMd(pathAbsolute) === '.md') {
-    arrayFile.push(pathAbsolute)
+
+
+const listFile = (road) => {
+  let filesArray = [];
+  const absoluteRoute = absolutePath(road);
+  if (verifyFileType(absoluteRoute)) {
+    if (pathIsMd(absoluteRoute) === '.md') {
+      filesArray.push(absoluteRoute)
+
+    }
+  } else {
+    //recorrer directorio
+    const items = readDir(absoluteRoute);
+    items.forEach(element => {
+      const pathJoin = path.join(absoluteRoute, element);
+      //console.log(pathJoin);
+      /*if (pathIsMd(pathJoin) === '.md') {
+        files.push(pathJoin);
+        files = files.concat(absoluteRoute);
+        console.log(typeof pathJoin);
+      } else {
+        
+      }*/
+      const newArray = listFile(pathJoin);
+      filesArray = filesArray.concat(newArray);
+
+    })
+
   }
-  console.log(arrayFile);
+
+  return filesArray;
+};
+
+
+const mdConvertHTML = (road) => {
+  let arrayObjects = [];
+  const newArrayFile = listFile(road);
+  newArrayFile.forEach(urlFile => {
+    const readMd = readFile(urlFile);
+    arrayObjects.push({
+      html: marked.parse(readMd),
+      urlFile: urlFile
+    });
+  });
+  return arrayObjects;
 }
+
+
+const getLink = (road) => {
+  let arrayObj = [];
+  const urlAndHTML = mdConvertHTML(road);
+  urlAndHTML.forEach(element => {
+    const $ = cheerio.load(element.html);
+    const links = $('a');
+    $(links).each(function (i, link) {
+      arrayObj.push({
+          href: $(link).attr('href'),
+          text: $(link).text(),
+          file: element.urlFile
+        });
+    });
+  });
+  return arrayObj;
+};
+/*let arrayLinks = []
+arrayHTML.forEach(element => {
+  const $ = cheerio.load(element.html);
+  const links = $('a');
+  $(links).each(function (i, link) {
+    arrayLinks.push(
+      {
+        href: $(link).attr('href'),
+        text: $(link).text(),
+        file: element.urlFile
+      }
+    )
+  });
+})
+console.log(arrayLinks);
+return arrayLinks;*/
+
+
+
+getLink('./md-link')
+
+
+
+
+
+
+
+
 
 
 
@@ -68,7 +142,9 @@ module.exports = {
   pahtIsDirectory,
   pathIsMd,
   readFile,
-  targetDir,
-  getFile
+  readDir,
+  listFile,
+  mdConvertHTML,
+  getLink
 };
 
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const apiMdLinks= require('./api');
 
-console.log(apiMdLinks.getFile('./md-link'));
-//console.log(apiMdLinks.readFile('./md-link/text.md'));
+//console.log(apiMdLinks.getFileMd('./md-link/text.md'));
+//console.log(apiMdLinks.readdirectory('./md-link'));
+//console.log(apiMdLinks.tourDirectory('./md-link'));
+//console.log(apiMdLinks.pahtIsDirectory('./md-link'));
+//console.log(apiMdLinks.readDir('./md-link'));
 
 
